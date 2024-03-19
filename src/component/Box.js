6b7c7a08b00a2cde34dd5b97910774d27bbbf47e
yujin1018 <115362555+yujin1018@users.noreply.github.com>
2024-03-19 22:19:29 +0900

    alt 추가

--- src/component/Box.js
+++ src/component/Box.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Box = (props) => {
   return (
-    <div className="box">
+    <div className="box" alt="가위바위보">
         <h1>{props.title}</h1>
         <img className="item-img" src={props.item && props.item.img}></img>
         <h2>Win</h2>
